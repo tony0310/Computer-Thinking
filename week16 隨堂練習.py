dee284a185dc993ca5ddb28264f5d89554fefca4
tony0310 <71623909+tony0310@users.noreply.github.com>
2020-12-28 14:12:24 +0800

    Add files via upload

---
+++ week16 隨堂練習.py
@@ -0,0 +1,39 @@
+print(True)
+print(False)
+
+print(bool(1))
+print(bool(0))
+print(bool(()))
+
+print(False + 1)
+
+score = 66
+score == 66
+
+name = "Francis"
+name == "Francis"
+name == "francis"
+
+5>2
+5 <2
+5 !=2
+
+
+True&True
+True&False
+False&True
+False&False
+True and False
+True or False
+False or True
+False|False
+not True
+not False
+
+x = 70
+print((x > 60) and (x < 80))
+print((x > 60) or (x < 80))
+print(not(x > 60))
+
+"H" in "Hello world" 
+"h" in "Hello world"
